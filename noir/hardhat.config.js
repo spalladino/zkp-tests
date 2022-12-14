cab2381aa4dddf450b45011b55d0e2fd16bf03c1
Santiago Palladino <spalladino@gmail.com>
2022-12-14 16:03:12 -0300

    Generate verifier contracts for noir

---
+++ noir/hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+      }
+    }
+  }
+};
